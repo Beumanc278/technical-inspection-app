47dfb4b47965e6f4edeb58b30ade920db3c0cf3e
dmitriy.anurin <anurin.dm.ig@gmail.com>
2022-01-17 18:44:33 +0300

    Fixed script js

--- application.py
+++ application.py
@@ -4,4 +4,8 @@ from flask_restful import Api
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource()
+@app.route('/')
+def home():
+    return render_template('Main.html')
+
+app.run(port=5000, debug=True)
