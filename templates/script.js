8bd750ee7496e582752e4e3108e59cdd5dd3a44a
sashka1850 <sashka1850@yandex.ru>
2022-01-17 18:23:12 +0300

    add js script

---
+++ templates/script.js
@@ -0,0 +1,16 @@
+console.log(document.getElementById("car-model"));
+const data = JSON.stringify({"car-model":document.getElementById("car-model").value});
+console.log(data);
+
+
+
+
+
+
+//xhr.send(data);
+
+/*$(function(){
+	console.log($('#client-info').text());
+});
+
+client-form.ob*/
