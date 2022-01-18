37ddcbb5990c6fcec3abc24eaa7bfbff1c764c9d
dmitriy.anurin <anurin.dm.ig@gmail.com>
2022-01-18 18:44:11 +0300

    Added files with testdata for creating database with mock data

--- models/database.py
+++ models/database.py
@@ -24,19 +24,25 @@ class CarDatabase:
         connection = sqlite3.connect(database_path)
         cursor = connection.cursor()
 
-        query = 'CREATE TABLE IF NOT EXISTS cars (id INTEGER PRIMARY KEY, "car-brend" text, "car-model" text, "car-year" text, "car-engine-type" text, "car-engine-capacity" text, "car-transmission" text, "car-drive-unit" text)'
+        query = 'CREATE TABLE IF NOT EXISTS cars (id INTEGER PRIMARY KEY, "car-brend" text, "car-model" text, "car-year" int, "car-engine-type" text, "car-engine-capacity" float, "car-transmission" text, "car-drive-unit" text)'
         cursor.execute(query)
 
         connection.commit()
         connection.close()
 
-    def insert_mock_car(self):
+    def insert_mock_cars(self):
         connection = sqlite3.connect(database_path)
         cursor = connection.cursor()
 
-        if not cursor.execute("SELECT * FROM cars WHERE id = 1").fetchone():
-            query = 'INSERT INTO cars VALUES (1, "Lexus", "GS300", "2006", "Бензин", "3.0", "АКПП", "Задний")'
-            cursor.execute(query)
+        cars_to_input = []
+        with open(r'../tests/test_car_data.txt', encoding='utf-8') as file:
+            for line in file.readlines():
+                result = tuple(line.replace('\n', '').split(','))
+                cars_to_input.append(result)
+
+        query = 'INSERT INTO cars VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)'
+        for car_data in cars_to_input:
+            cursor.execute(query, car_data)
 
         connection.commit()
         connection.close()
@@ -51,6 +57,7 @@ class CarDatabase:
         query = f"SELECT * FROM cars{where_part}"
         result = cursor.execute(query)
         row = result.fetchone()
+        print(row)
 
         connection.close()
 
@@ -60,21 +67,57 @@ class ServiceDatabase:
         connection = sqlite3.connect(database_path)
         cursor = connection.cursor()
 
-        query = 'CREATE TABLE IF NOT EXISTS services (id INTEGER PRIMARY KEY, "service-description" text, "service-inspection-name" text, "service-inspection_cost" int, "service-car-id" int)'
+        query = 'CREATE TABLE IF NOT EXISTS services (id INTEGER PRIMARY KEY, "service-name" text, "service-description" text)'
         cursor.execute(query)
 
         connection.commit()
         connection.close()
 
-    def insert_mock_service(self):
+    def insert_mock_services(self):
         connection = sqlite3.connect(database_path)
         cursor = connection.cursor()
 
-        query = 'INSERT INTO services VALUES (1, "Пиздатый сервис", "ТО для новичков", "100500", "1")'
+        services_to_input = []
+        with open('../tests/test_service_data.txt', encoding='utf-8') as file:
+            for line in file.readlines():
+                result = line.replace('\n', '').replace('"', '').split(',')
+                services_to_input.append(result)
+
+        query = "INSERT INTO services VALUES (NULL, ?, ?)"
+        for service in services_to_input:
+            cursor.execute(query, service)
+
+        connection.commit()
+        connection.close()
+
+class InspectionDatabase:
+
+    def create_table(self):
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+
+        query = 'CREATE TABLE IF NOT EXISTS inspections (id INTEGER PRIMARY KEY, "inspection-name" text, "inspection-cost" float, "mileage" float, "lifetime" int, "service-id" int, "car-parameters" text)'
         cursor.execute(query)
 
-        query = 'INSERT INTO services VALUES (2, "Не самый пиздатый сервис", "ТО для профессионалов", "200000", "1")'
-        cursor.execute(query)
+        connection.commit()
+        connection.close()
+
+    def insert_mock_inspections(self):
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+
+        inspections_to_input = []
+        with open('../tests/test_inspection_data.txt', encoding='utf-8') as file:
+            for line in file.readlines():
+                result = line.replace('\n', '')
+                car_parameters = result[result.find('"'):result.rfind('"') + 1]
+                other_line = result[:result.find('"')]
+                result = (*other_line.split(',')[:-1], car_parameters)
+                inspections_to_input.append(result)
+
+        query = 'INSERT INTO inspections VALUES (NULL, ?, ?, ?, ?, ?, ?)'
+        for inspection in inspections_to_input:
+            cursor.execute(query, inspection)
 
         connection.commit()
         connection.close()
@@ -83,6 +126,8 @@ class ServiceDatabase:
 UserDatabase().create_table()
 CarDatabase().create_table()
 ServiceDatabase().create_table()
+InspectionDatabase().create_table()
 
-CarDatabase().insert_mock_car()
-ServiceDatabase().insert_mock_service()
+CarDatabase().insert_mock_cars()
+ServiceDatabase().insert_mock_services()
+InspectionDatabase().insert_mock_inspections()
