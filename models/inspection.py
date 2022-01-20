087d990109ccc069fd31980df22cb241ac8bcef7
dmitriy.anurin <anurin.dm.ig@gmail.com>
2022-01-20 12:11:43 +0300

    Added insert, update and delete methods for inspection entity

--- models/inspection.py
+++ models/inspection.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import Tuple
 
 from config import database_path
 from utils import create_filter
@@ -14,11 +15,14 @@ class InspectionModel:
         self.mileage = mileage
         self.lifetime = lifetime
         self.service_id = service_id
-        self.car_parameters = car_parameters
+        if isinstance(car_parameters, dict):
+            self.car_parameters = self.transform_car_parameters_to_string(car_parameters)
+        else:
+            self.car_parameters = car_parameters
 
     def json(self):
         return {
-            'insection-id': self.id,
+            'inspection-id': self.id,
             'inspection-name': self.name,
             'inspection-cost': self.cost,
             'inspection-mileage': self.mileage,
@@ -28,12 +32,25 @@ class InspectionModel:
         }
 
     @classmethod
+    def get_by_id(cls, inspection_id: int):
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+
+        query = f'SELECT * FROM inspections WHERE "inspection-id" = {inspection_id}'
+        print(f'Sending query: {query}')
+        result = cursor.execute(query).fetchone()
+        if result:
+            return cls(*result).json()
+        else:
+            return None
+
+    @classmethod
     def get_inspections_by_car_parameters(cls, car_parameters: dict):
         string_interpretation = ','.join([str(element) for element in list(car_parameters.values())[1:]])
         connection = sqlite3.connect(database_path)
         cursor = connection.cursor()
 
-        query = f'SELECT * FROM inspections WHERE "car-parameters" = "{string_interpretation}"'
+        query = f'SELECT * FROM inspections WHERE "inspection-car-parameters" = "{string_interpretation}"'
         print(f'Sending query: {query}')
         result = cursor.execute(query)
         inspections = []
@@ -54,6 +71,87 @@ class InspectionModel:
         for row in result:
             inspections.append(cls(*row).json())
         return inspections
+
+    def insert_to_database(self) -> Tuple[bool, int]:
+        parameters = self.json()
+        parameters.pop('inspection-id')
+
+        inspection_id = self.is_inspection_exists(parameters)
+        if inspection_id is not None:
+            return False, inspection_id
+
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+
+        print(f'parameters for paste {parameters}')
+        query = 'INSERT INTO inspections VALUES (NULL, ?, ?, ?, ?, ?, ?)'
+        cursor.execute(query, (tuple(parameters.values())))
+        print(f'Sending query: {query}')
+
+        connection.commit()
+        connection.close()
+        print('INSERT query was completed successfully.')
+
+        new_inspection_id = self.is_inspection_exists(parameters)
+        return True, new_inspection_id
+
+    def update_in_database(self, received_data: dict):
+        received_data['inspection-car-parameters'] = self.transform_car_parameters_to_string(received_data['inspection-car-parameters'])
+        parameters_to_change = {}
+        existing_service = self.get_by_id(received_data['inspection-id'])
+        for parameter, value in received_data.items():
+            if value != existing_service[parameter]:
+                parameters_to_change[parameter] = value
+        if not parameters_to_change:
+            return False, received_data['inspection-id']
+
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+
+        query = f'UPDATE inspections SET {create_filter(parameters_to_change, operation="UPDATE")} WHERE "inspection-id" = {received_data["inspection-id"]}'
+        print(f'Sending query: {query}')
+        cursor.execute(query)
+        result = cursor.execute(f'SELECT * FROM inspections WHERE "inspection-id" = {received_data["inspection-id"]}').fetchone()
+        updated_inspection = InspectionModel(*result)
+
+        connection.commit()
+        connection.close()
+        print('UPDATE query was completed successfully.')
+        return True, updated_inspection
+
+    def delete_inspection(self):
+        if not self.is_inspection_exists(self.json()):
+            return None
+        deleted_inspection = InspectionModel(*self.get_by_id(self.json()['inspection-id']).values())
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+
+        query = f'DELETE FROM inspections WHERE "inspection-id" = {self.id}'
+        cursor.execute(query)
+        print(f'Sending query: {query}')
+
+        connection.commit()
+        connection.close()
+        print('DELETE query was completed successfully.')
+        return deleted_inspection
+
+    def is_inspection_exists(self, parameters: dict) -> [int, None]:
+        inspections = self.get_inspections_by_parameters(parameters)
+        if inspections:
+            existing_inspection = inspections[0]
+            return existing_inspection['inspection-id']
+        return None
+
+    def transform_car_parameters_to_string(self, car_parameters: dict) -> str:
+        output_string = ''
+        if 'car-id' in car_parameters.keys():
+            car_parameters.pop('car-id')
+        for value in car_parameters.values():
+            if not output_string:
+                output_string = value
+            else:
+                output_string += f',{value}'
+        return output_string
 
 class InspectionListModel:
 
--- resources/inspection.py
+++ resources/inspection.py
@@ -5,11 +5,13 @@ from models.inspection import InspectionListModel, InspectionModel
 
 class Inspection(Resource):
     parser = reqparse.RequestParser()
+    parser.add_argument('inspection-id', type=int)
+    parser.add_argument('inspection-name', type=str)
     parser.add_argument('inspection-cost', type=int)
-    parser.add_argument('mileage', type=int)
-    parser.add_argument('lifetime', type=int)
-    parser.add_argument('service-id', type=int)
-    parser.add_argument('car-parameters', type=dict)
+    parser.add_argument('inspection-mileage', type=int)
+    parser.add_argument('inspection-lifetime', type=int)
+    parser.add_argument('inspection-service-id', type=int)
+    parser.add_argument('inspection-car-parameters', type=dict)
 
     def get(self):
         return {"fields-for-request": InspectionModel().json()}
@@ -20,14 +22,47 @@ class Inspection(Resource):
         valid_values = [value is not None for value in data.values()]
         if True not in valid_values:
             return {"message": f"The parameters which have been given are empty - {data}"}, 400
-        if 'car-parameters' in data.keys() and data['car-parameters']:
-            inspections = InspectionModel.get_inspections_by_car_parameters(data['car-parameters'])
+        if 'inspection-car-parameters' in data.keys() and data['inspection-car-parameters']:
+            inspections = InspectionModel.get_inspections_by_car_parameters(data['inspection-car-parameters'])
         else:
             inspections = InspectionModel.get_inspections_by_parameters(data)
         if inspections:
             return {"inspections": inspections}
         else:
             return {"message": f"No inspections were found for parameters {data}"}, 404
+
+    def put(self):
+        data = Inspection.parser.parse_args()
+        print(f"Received data: {data}")
+        valid_values = [value is not None for value in data.values()]
+        if True not in valid_values:
+            return {"message": f"The parameters which have been given are empty - {data}"}, 400
+        if data['inspection-id']:
+            result, updated_inspection = InspectionModel(*data.values()).update_in_database(data)
+            if result:
+                return {"message": "Inspection was updated successfully", "inspection-parameters": updated_inspection.json()}, 201
+            else:
+                return {"message": f"Inspection was not updated with given parameters {data}."}, 500
+        else:
+            result, inspection_id = InspectionModel(*data.values()).insert_to_database()
+            if result:
+                return {"message": "Inspection was inserted successfully.", "inspection-id": inspection_id}, 201
+            else:
+                return {"message": f"Inspection with given parameters already exists with the inspection ID - {inspection_id}",
+                        "inspection-id": inspection_id}
+
+    def delete(self):
+        data = Inspection.parser.parse_args()
+        print(f"Received data: {data}")
+        valid_values = [value is not None for value in data.values()]
+        if True not in valid_values:
+            return {"message": f"The parameters which have been given are empty - {data}"}, 400
+        deleted_inspection = InspectionModel(*data.values()).delete_inspection()
+        if deleted_inspection:
+            return {"message": f"The inspection with ID {data['inspection-id']} was deleted successfully.",
+                    "inspection-parameters": deleted_inspection.json()}, 204
+        else:
+            return {"message": f'The inspection with ID {data["inspection-id"]} does not exist.'}, 400
 
 class InspectionsList(Resource):
 
